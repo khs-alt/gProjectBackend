a9a79e38742faaf0f52247cbf9d6b40387f69e32
khs-alt <aa_aa_aa_11@naver.com>
2023-12-01 05:02:55 +0900

    add session function

--- app/handler/sessionHandler.go
+++ app/handler/sessionHandler.go
@@ -2,11 +2,13 @@ package handler
 
 import (
 	"GoogleProjectBackend/util"
+	"fmt"
 	"net/http"
 )
 
 func SessionAuthMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("SessionAuthMiddleware")
 		session, err := util.Store.Get(r, "survaySession")
 		if err != nil {
 			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
