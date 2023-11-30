9f8c21f4813a80408e1f9a4d4209e424850a9194
khs-alt <aa_aa_aa_11@naver.com>
2023-11-30 12:39:01 +0900

    change random chracter: delete special charater

--- util/makeRandomString.go
+++ util/makeRandomString.go
@@ -8,7 +8,7 @@ import (
 
 // 랜덤 문자열을 생성하는 함수
 func GenerateRandomString(n int) string {
-	const letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()"
+	const letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
 	rand.Seed(time.Now().UnixNano())
 	var sb strings.Builder
 	for i := 0; i < n; i++ {
